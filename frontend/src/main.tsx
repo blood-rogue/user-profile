f07b97f5088c191e0677eb671d565ea68b337485
Blood Rogue <64477767+blood-rogue@users.noreply.github.com>
2024-02-03 16:47:37 +0530

    Added 'Inter' font

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 
 import "./interceptor";
+import "@fontsource-variable/inter";
 import { App } from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(<App />);
